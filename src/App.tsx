93841833e09f280bfb76a17469ef47345fd95366
Himenon <k.himeno314@gmail.com>
2022-04-21 11:42:30 +0900

    chore: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import * as React from "react";
 import { Styles } from "./App.Styles";
-import Editor from "@monaco-editor/react";
+import Editor, { Monaco } from "@monaco-editor/react";
+import type * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 
 const dummyCode = `export const sum = (list: (number | string)[]) => {
   return list.reduce<number>((total, value) => {
@@ -16,7 +18,53 @@ const dummyCode = `export const sum = (list: (number | string)[]) => {
 };
 `;
 
+interface MonacoEditorRef {
+  editor: monaco.editor.IStandaloneCodeEditor;
+  monaco: Monaco;
+}
+
+/**
+ * @see https://stackoverflow.com/questions/68342605/monaco-editor-deltadecorations-changes-the-style-of-the-whole-text-instead-of-ju
+ */
+const generateRangeError = ({ editor, monaco }: MonacoEditorRef) => {
+  console.log("hello");
+  // editor.deltaDecorations(
+  //   [],
+  //   [
+  //     {
+  //       range: new monaco.Range(1, 3, 3, 1),
+  //       options: {
+  //         isWholeLine: true,
+  //         className: "myContentClass",
+  //         glyphMarginClassName: "myGlyphMarginClass",
+  //       },
+  //     },
+  //   ]
+  // );
+  const textModel = editor.getModel();
+  if (!textModel) {
+    return;
+  }
+  monaco.editor.setModelMarkers(textModel, "test", [
+    {
+      startLineNumber: 2,
+      startColumn: 1,
+      endLineNumber: 2,
+      endColumn: 10,
+      message: "Hello world!",
+      severity: monaco.MarkerSeverity.Warning,
+    },
+  ]);
+};
+
 function App() {
+  const editorRef = React.useRef<MonacoEditorRef | null>(null);
+  React.useEffect(() => {
+    if (!editorRef.current) {
+      return;
+    }
+    generateRangeError(editorRef.current);
+  }, [editorRef]);
   return (
     <Styles>
       <Editor
@@ -24,6 +72,12 @@ function App() {
         theme="vs-dark"
         defaultLanguage="typescript"
         defaultValue={dummyCode}
+        onMount={(editor, monaco) => {
+          editorRef.current = {
+            editor,
+            monaco,
+          };
+        }}
       />
     </Styles>
   );
