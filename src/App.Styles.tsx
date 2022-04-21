e30f2c5ff443c0dd442447218e9d9bf25e7eb6c0
Himenon <k.himeno314@gmail.com>
2022-04-21 11:12:24 +0900

    chore: update

---
+++ src/App.Styles.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Styles = styled.div`
+  width: 100%;
+  height: 100%;
+  #container {
+    width: 100%;
+    height: 100%
+  }
+`;
---
+++ src/Global.Styles.tsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html, body {
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    height: 100%
+  }
+  #root {
+    width: 100%;
+    height: 100%
+  }
+`;
